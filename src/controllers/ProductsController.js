84277a6c4b577b21e310475ff26b63574d9f84e9
Karina Formentini Dinella <kaformentini@gmail.com>
2020-06-19 09:59:58 -0300

    wip create product

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -6,26 +6,27 @@ const db = lowdb(adapter);
 
 module.exports = {
   async index(req, res) {
-    console.log("aqui");
-    const products = db.get('products').value();
+    // console.log("aqui");
+    const products = db.get("products").value();
     return res.json(products);
   },
+
+  async create(req, res) {
+    const product = req.body;
+    console.log(product);
+    db.get("products")
+      .push({
+        name: product.name,
+        id: product.id,
+        amount: product.amount,
+        description: product.description,
+      })
+      .write();
+  },
+
+  // async update(req, res) {
+  //   const queryProduct = req.body
+  //   db.get("products").find({queryProduct.find}).assign({queryProduct.change});
+  // }
+    
 };
-
-// function getProducts(){
-//   console.log("aqui2");
-
-//   const products = db.get('product').value();
-//   return products
-// }
-
-function createProduct() {
-  db.get("product")
-    .push({
-      name: "cadeira",
-      id: 01,
-      amount: 20,
-      description: "vermelha",
-    })
-    .write();
-}
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const bodyParser = require('body-parser')
+
 const routes = express.Router();
+// routes.use(bodyParser.json())
 
 const ProductsController = require('./controllers/ProductsController');
 
 routes.get("/products", (ProductsController.index));
+
+routes.post("/products", (ProductsController.create));
 
 module.exports = routes;
